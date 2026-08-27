a885e93131d593cbbce8f116f1f5fc8d2db131a5
danova <62123153+DANOVACAVALARIA@users.noreply.github.com>
2026-08-27 13:54:36 -0300

    Configure SSL mode for database connection
    Add SSL mode configuration for database connection.

--- database/db.go
+++ database/db.go
@@ -14,8 +14,12 @@ var (
 )
 
 func ConectaComBancoDeDados() {
+	sslMode := os.Getenv("SSLMODE")
+	if sslMode == "" {
+	    sslMode = "disable" // padrão para ambiente local/testes
+	}
 	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=require"
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")++ " sslmode=" + sslMode
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
